4e17e8205622539ddb0966916273db864d798548
Cinxdy <cinxdys2@gmail.com>
2020-10-25 10:51:05 +0900

    modified UDPServer

--- UDPServer.py
+++ UDPServer.py
@@ -1,7 +1,7 @@
 from socket import *
 serverPort = 12000
 serverSocket = socket(AF_INET, SOCK_DGRAM)
-serverSocket.bind('',serverPort)
+serverSocket.bind(('',serverPort))
 print("The server is ready to receive")
 while True:
     message, clientAddress = serverSocket.recvfrom(2048)
